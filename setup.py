1132d7ccdbcc645f7dd74f6619db550168a69c5c
Yijun Yuan <Jarrome@users.noreply.github.com>
2022-06-30 15:17:15 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='IFR',
+      version='0.1',
+      description='Indirect Registration',
+      author='Yijun Yuan',
+      url='https://github.com/Jarrome/IFR',
+      packages=['IFR'],
+      package_dir={'IFR':'./'},
+     )
